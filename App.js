bbd327adac0f2d8e9070d21476e4183380ff1270
Treyton Davis <37282419+tdavis422@users.noreply.github.com>
2021-02-15 16:36:10 -0600

    Added files

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
+import {
+	Modal,
+	Platform,
+	StyleSheet,
+	View,
+} from 'react-native';
+import Constants from 'expo-constants';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Feed from './screens/Feed';
+import Comments from './screens/Comments';
+
+export default class App extends React.Component {
+	state = {
+		commentsForItem: {},
+		showModal: false,
+		selectedItemId: null,
+	};
+	
+	openCommentScreen = id => {
+		this.setState({
+			showModal: true,
+			selectedItemId: id,
+		});
+	};
+	
+	closeCommentScreen = () => {
+		this.setState({
+			showModal: false,
+			selectedItemId: null,
+		});
+	};
+	
+	onSubmitComment = (text) => {
+		const { selectedItemId, commentsForItem } = this.state;
+		const comments = commentsForItem[selectedItemId] || [];
+	};
+	
+	render() {
+		const {commentsForItem, showModal, selectedItemId } = this.state;
+		
+		return (
+			<View style={styles.container}>
+				<Feed 
+				style={styles.feed}
+				commentsForItem={commentsForItem}
+				onPressComments={this.openCommentSection}
+				/>
+				
+				<Modal
+				visible={showModal}
+				animationType="slide"
+				onRequestClose={this.closeCommentScreen}
+				>
+					<Comments
+						style={styles.container}
+						comments={commentsForItem[selectedItemId] || []}
+						onClose={this.closeCommentScreen}
+					/>
+				</Modal>
+			</View>
+		);
+	}
 }
+
+const platformVersion =
+	Platform.OS === 'ios'
+	? parseInt(Platform.Version, 10)
+	: Platform.Version;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+	flex: 1,
+	backgroundColor: '#fff',
+  },
+  
+  feed: {
+	  flex: 1,
+	  marginTop:
+		Platform.OS==='android' || platformVersion < 11
+		? Constants.statusBarHeight
+		: 0,
+  },
+  
+  comments: {
+	  flex: 1,
+	  marginTop:
+		Platform.OS === 'ios' && platformVersion < 11
+		? Constants.statusBarHeight
+		: 0,
   },
 });
